b70628619abbed55a9de5867ea3fa8a1e968c307
paul <paulentz@gmail.com>
2025-07-04 17:50:31 -0500

    first commit of sql challenge

--- insert.sql
+++ insert.sql
@@ -1,9 +1,3 @@
-# STL Big Data Meetup
-
-An example SQL coding challenge for the STL Big Data Meetup in May 2025
-
-# Setup
-
 ```
 -- Create the Attractions table
 CREATE TABLE Attractions (
@@ -50,20 +44,3 @@ INSERT INTO Reviews (ReviewID, AttractionID, UserID, ReviewDate, Rating, Comment
 (8, 1, 105, '2025-04-22', 3, 'The museum at the base is interesting.'),
 (9, 7, 101, '2025-04-22', 5, 'Best frozen custard ever!'),
 (10, 3, 104, '2025-04-23', 4, 'Lots of different animals to see.');
-```
-
-# Challenges
-
-**Question 1**: Retrieve the names of all attractions located in the city of 'St. Louis'.
-
-**Question 2**: List the names of attractions along with the number of reviews they have received. Order the results from the attraction with the most reviews to the least.
-
-**Question 3**: Find the average rating for each attraction. Display the attraction name and its average rating, rounded to two decimal places.
-
-**Question 4**: Identify the names of attractions that have at least one review with a rating of 5 and at least one review with a rating of 3.
-
-**Question 5**: For each attraction category, find the attraction with the highest average rating. Display the category, attraction name, and its average rating. (Hint: You might need to use window functions or a subquery).
-
-# Helpful Resources
-
-https://datalemur.com/sql-tutorial
